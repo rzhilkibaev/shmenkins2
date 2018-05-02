252d86a73292d45a061c81ddda0142bde246d4ee
Renat <rzhilkibaev@gmail.com>
2018-05-01 21:23:41 -0700

    Notify when build is scheduled

--- shmenkins2/ScheduleBuild.py
+++ shmenkins2/ScheduleBuild.py
@@ -1,12 +1,18 @@
-import logging
 import json
+import logging
+import os
+
+import boto3
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+build_scheduled_topic = boto3.resource('sns').Topic(os.environ["BUILD_SCHEDULED_TOPIC_ARN"])
+
 
 def run(event, context):
-    logger.info(f"start; event={json.dumps(event)}")
-    logger.info(f"Scheduling build")
-    logger.info(f"finish")
+    sns_record = event["Records"][0]["Sns"]
+    message = json.loads(sns_record["Message"])
+    url = message["url"]
+    build_scheduled_topic.publish(Message=json.dumps({"url": url}))
