f2fe49ab4cfe8a5e70f0fe2ab390834588e79732
Renat <rzhilkibaev@gmail.com>
2018-07-07 18:30:51 -0700

    Enqueue artifact-updated event on scm change

--- shmenkins2/HandlePushNotification.py
+++ shmenkins2/HandlePushNotification.py
@@ -1,13 +1,14 @@
-import os
-import logging
-import boto3
 import json
+import logging
+import os
+
+import boto3
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-build_requested_topic = boto3.resource('sns').Topic(os.environ["BUILD_REQUESTED_TOPIC_ARN"])
+artifact_updated_queue = boto3.resource('sqs').Queue(os.environ["ARTIFACT_UPDATED_QUEUE_URL"])
 
 
 def run(event, context):
@@ -16,7 +17,7 @@ def run(event, context):
     body = json.loads(event["body"])
     url = body["repository"]["url"]
 
-    build_requested_topic.publish(Message=json.dumps({"url": url}))
+    artifact_updated_queue.send_message(MessageBody=json.dumps({"url": url}))
 
     logger.info(f"finish")
     return {"statusCode": 201, "body": ""}
